8bbaba30e76ffb06eacb03652817a220b401deca
Satish Paidikondala <satishpaidikondala@example.com>
2026-06-29 12:06:12 +0530

    Call setupEvalInterface at module load time

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { setupEvalInterface } from './utils/eval'
+
+setupEvalInterface()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
