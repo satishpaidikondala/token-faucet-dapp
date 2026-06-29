4cbbe05c8d16eee6098f14d832228e816a02cbb6
Satish Paidikondala <satishpaidikondala@example.com>
2026-06-29 12:06:27 +0530

    Add localhost network to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,9 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
